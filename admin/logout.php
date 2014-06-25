d93b8a82a057302de119bca3e89c21c664b626fb
Colby Ham <colbyiphone@gmail.com>
2014-06-24 21:25:02 -0700

    Delete Admin/Databse Dir's
    Unecessary

--- admin/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Init session
-session_start();
-// Delete current session
-unset($_SESSION['username']);
-// Jump to login page
-header('Location: index.php'); 
-// It’ll take us back, logged out
-?>
--- database/error.php
+++
@@ -1,6 +0,0 @@
-<?php include "http://people.oregonstate.edu/~hamc/cs275/header.htm"; ?> 
-<div class = "center">
-<h3>Error Updating/Adding Artist</h3>
-<a href="http://people.oregonstate.edu/~hamc/cs275/database/index.php">Try Again</a>
-</div>
-<?php include "http://people.oregonstate.edu/~hamc/cs275/footer.htm"; ?> 
